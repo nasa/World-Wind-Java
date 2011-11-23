158d294b6142ef00e30f43c0ab14722f6d6b3c6e
pabercrombie <pabercrombie@1c351a6f-456c-4935-88af-207f26c224cf>
2011-11-23 19:29:21 +0000

    Cleaned up Symbology example. Increased default window size, rearranged graphics so that they do not overlap, and separated tactical graphics and tactical symbols onto different layers.

--- WorldWind/src/gov/nasa/worldwindx/examples/Symbology.java
+++ WorldWind/src/gov/nasa/worldwindx/examples/Symbology.java
@@ -14,9 +14,12 @@ import gov.nasa.worldwind.render.SurfaceImage;
 import gov.nasa.worldwind.symbology.TacticalGraphic;
 import gov.nasa.worldwind.symbology.milstd1477.MilStd1477IconRetriever;
 import gov.nasa.worldwind.symbology.milstd2525.*;
+import gov.nasa.worldwind.util.WWUtil;
 
+import java.awt.*;
 import java.awt.image.*;
 import java.util.*;
+import java.util.List;
 
 /**
  * Demonstrates how to create and render symbols from the MIL-STD-2525 symbol set. See the <a title="Symbology Usage
@@ -34,11 +37,36 @@ public class Symbology extends ApplicationTemplate
         {
             super(true, true, false);
 
-            RenderableLayer layer = new RenderableLayer();
+            RenderableLayer symbolLayer = new RenderableLayer();
+            symbolLayer.setName("Tactical Symbols");
 
-            // Create tactical graphics and add them to the layer
-            this.createGraphics(layer);
+            RenderableLayer lineLayer = new RenderableLayer();
+            lineLayer.setName("Tactical Graphics (Lines)");
 
+            RenderableLayer areaLayer = new RenderableLayer();
+            areaLayer.setName("Tactical Graphics (Areas)");
+
+            // Create tactical symbols and graphics and add them to the layer
+            this.createSymbols(symbolLayer);
+            this.createLineGraphics(lineLayer);
+            this.createAreaGraphics(areaLayer);
+
+            insertBeforePlacenames(getWwd(), symbolLayer);
+            insertBeforePlacenames(getWwd(), lineLayer);
+            insertBeforePlacenames(getWwd(), areaLayer);
+
+            this.getLayerPanel().update(this.getWwd());
+
+            // Size the World Window to provide enough screen space for the graphics, and center the World Window
+            // on the screen.
+            Dimension size = new Dimension(1200, 800);
+            this.setPreferredSize(size);
+            this.pack();
+            WWUtil.alignComponent(null, this, AVKey.CENTER);
+        }
+
+        protected void createSymbols(RenderableLayer layer)
+        {
             // Display a MIL-STD2525 tactical icon
             //      Warfighting
             String URL = "http://worldwindserver.net/milstd2525/";
@@ -60,8 +88,8 @@ public class Symbology extends ApplicationTemplate
 
             //      Stability Operations
             img = symGen.createIcon("OHOPYT---------");
-            s = new Sector(Angle.fromDegrees(34.58), Angle.fromDegrees(34.48),
-                Angle.fromDegrees(-117.7), Angle.fromDegrees(-117.57));
+            s = new Sector(Angle.fromDegrees(34.8), Angle.fromDegrees(34.7),
+                Angle.fromDegrees(-117.55), Angle.fromDegrees(-117.42));
             symbol = new SurfaceImage(img, s);
             layer.addRenderable(symbol);
 
@@ -82,20 +110,22 @@ public class Symbology extends ApplicationTemplate
                 Angle.fromDegrees(-117.9), Angle.fromDegrees(-117.77));
             symbol = new SurfaceImage(img, s);
             layer.addRenderable(symbol);
-
-            this.getWwd().getModel().getLayers().add(layer);
         }
 
-        protected void createGraphics(RenderableLayer layer)
+        protected void createLineGraphics(RenderableLayer layer)
         {
             MilStd2525GraphicFactory factory = new MilStd2525GraphicFactory();
             MilStd2525TacticalGraphic graphic;
 
+            /////////////////////////////////////////////
+            // Phase line (2.X.2.1.2.4)
+            /////////////////////////////////////////////
+
             // Create a Friendly Phase Line
             List<Position> positions = Arrays.asList(
-                Position.fromDegrees(34.8221, -117.8043, 0),
-                Position.fromDegrees(34.8235, -117.7196, 0)
-            );
+                Position.fromDegrees(34.9349, -117.6303, 0),
+                Position.fromDegrees(34.9843, -117.5885, 0),
+                Position.fromDegrees(34.9961, -117.4891, 0));
             graphic = factory.createGraphic("GFGPGLP----AUSX", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Phase line: friendly");
             graphic.setText("A");
@@ -103,9 +133,9 @@ public class Symbology extends ApplicationTemplate
 
             // Create a Hostile Phase Line
             positions = Arrays.asList(
-                Position.fromDegrees(34.7327, -117.8347, 0),
-                Position.fromDegrees(34.7328, -117.7305, 0)
-            );
+                Position.fromDegrees(34.9190, -117.5919, 0),
+                Position.fromDegrees(34.9589, -117.5618, 0),
+                Position.fromDegrees(34.9701, -117.4798, 0));
             graphic = factory.createGraphic("GHGPGLP----AUSX", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Phase line: hostile");
             graphic.setText("B");
@@ -113,108 +143,72 @@ public class Symbology extends ApplicationTemplate
 
             // Create a Hostile, Anticipated, Phase Line
             positions = Arrays.asList(
-                Position.fromDegrees(34.8052, -117.8769, 0),
-                Position.fromDegrees(34.7445, -117.9252, 0)
-            );
+                Position.fromDegrees(34.8910, -117.5726, 0),
+                Position.fromDegrees(34.9367, -117.5354, 0),
+                Position.fromDegrees(34.9480, -117.4741, 0));
             graphic = factory.createGraphic("GHGAGLP----AUSX", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Phase line: hostile (anticipated)");
             graphic.setText("C");
             layer.addRenderable(graphic);
 
-            // Create a General Area
-            positions = Arrays.asList(
-                Position.fromDegrees(34.8193, -117.6454, 0),
-                Position.fromDegrees(34.8043, -117.5682, 0),
-                Position.fromDegrees(34.7800, -117.6040, 0),
-                Position.fromDegrees(34.7819, -117.6687, 0)
-            );
-            graphic = factory.createGraphic("GHGPGAG----AUSX", positions, null);
-            graphic.setValue(AVKey.DISPLAY_NAME, "General Area");
-            graphic.setText("Area");
-            layer.addRenderable(graphic);
+            /////////////////////////////////////////////
+            // Main Attack (2.X.2.5.2.1.4.1)
+            /////////////////////////////////////////////
 
-            // Create an Airfield Zone
-            positions = Arrays.asList(
-                Position.fromDegrees(34.7152, -117.6526, 0),
-                Position.fromDegrees(34.7219, -117.6089, 0),
-                Position.fromDegrees(34.6918, -117.5904, 0),
-                Position.fromDegrees(34.6818, -117.6665, 0)
-            );
-            graphic = factory.createGraphic("GFGPGAZ----AUSX", positions, null);
-            graphic.setValue(AVKey.DISPLAY_NAME, "Airfield Zone");
-            layer.addRenderable(graphic);
-
-            // Create a Deception graphic
-            positions = Arrays.asList(
-                Position.fromDegrees(34.6665, -117.9306, 0),
-                Position.fromDegrees(34.6476, -117.9601, 0),
-                Position.fromDegrees(34.6426, -117.9020, 0)
-            );
-            graphic = factory.createGraphic("GHGPPD-----AUSX", positions, null);
-            graphic.setValue(AVKey.DISPLAY_NAME, "Dummy (Deception/Decoy)");
-            layer.addRenderable(graphic);
-
-            // Create a Restricted Operation Zone
-            positions = Arrays.asList(
-                Position.fromDegrees(34.9442, -117.5815, 0),
-                Position.fromDegrees(34.8581, -117.5842, 0),
-                Position.fromDegrees(34.8676, -117.4088, 0),
-                Position.fromDegrees(34.9112, -117.3972, 0),
-                Position.fromDegrees(34.9522, -117.4658, 0)
-            );
-            graphic = factory.createGraphic("GFGPAAR----AUSX", positions, null);
-            graphic.setValue(AVKey.DISPLAY_NAME, "Restricted Operations Zone (ROZ)");
-            graphic.setText("(Unit ID)");
-            graphic.setModifier(AVKey.DATE_TIME, Arrays.asList(new Date(), new Date()));
-            graphic.setModifier(AVKey.ALTITUDE, Arrays.asList("100 FT AGL", "1000 FT AGL"));
-            layer.addRenderable(graphic);
-
-            // Create a Main Attack graphic
             positions = Arrays.asList(
                 Position.fromDegrees(34.4643, -117.7323, 0), // Pt. 1: Tip of the arrow
                 Position.fromDegrees(34.4962, -117.7808, 0), // Pt. 2: First path control point
                 Position.fromDegrees(34.4934, -117.8444, 0), // Pt. N - 1: Last path control point
                 Position.fromDegrees(34.4602, -117.8570, 0),
-                Position.fromDegrees(34.4844, -117.7303, 0) // Pt. N: Width of the arrow head
-            );
+                Position.fromDegrees(34.4844, -117.7303, 0)); // Pt. N: Width of the arrow head
             graphic = factory.createGraphic("GFGPOLAGM-----X", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Main Attack");
             layer.addRenderable(graphic);
 
-            // Create an Rotary Wing graphic
+            /////////////////////////////////////////////
+            // Attack, rotary wing (2.X.2.5.2.1.3)
+            /////////////////////////////////////////////
+
             positions = Arrays.asList(
                 Position.fromDegrees(34.5610, -117.4541, 0),
                 Position.fromDegrees(34.5614, -117.5852, 0),
                 Position.fromDegrees(34.5287, -117.6363, 0),
                 Position.fromDegrees(34.4726, -117.6363, 0),
-                Position.fromDegrees(34.5820, -117.4700, 0)
-            );
+                Position.fromDegrees(34.5820, -117.4700, 0));
             graphic = factory.createGraphic("GFGPOLAR------X", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Attack, Rotary Wing");
             layer.addRenderable(graphic);
 
-            // Create an Aviation offensive graphic
+            /////////////////////////////////////////////
+            // Aviation axis of advance (2.X.2.5.2.1.1)
+            /////////////////////////////////////////////
+
             positions = Arrays.asList(
                 Position.fromDegrees(34.5437, -117.8007, 0),
                 Position.fromDegrees(34.5535, -117.9256, 0),
                 Position.fromDegrees(34.6051, -117.9707, 0),
-                Position.fromDegrees(34.5643, -117.8219, 0)
-            );
+                Position.fromDegrees(34.5643, -117.8219, 0));
             graphic = factory.createGraphic("GFGPOLAV------X", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Aviation");
             layer.addRenderable(graphic);
 
-            // Create a Supporting Attack graphic
+            /////////////////////////////////////////////
+            // Supporting attack (2.X.2.5.2.1.4.2)
+            /////////////////////////////////////////////
+
             positions = Arrays.asList(
                 Position.fromDegrees(34.4980, -117.5541, 0),
                 Position.fromDegrees(34.4951, -117.4667, 0),
                 Position.fromDegrees(34.4733, -117.4303, 0),
                 Position.fromDegrees(34.4217, -117.4056, 0),
-                Position.fromDegrees(34.4780, -117.53, 0)
-            );
+                Position.fromDegrees(34.4780, -117.53, 0));
             graphic = factory.createGraphic("GFGPOLAGS-----X", positions, null);
             graphic.setValue(AVKey.DISPLAY_NAME, "Supporting Attack");
             layer.addRenderable(graphic);
+
+            /////////////////////////////////////////////
+            // Minimum risk route (2.X.2.2.2.2)
+            /////////////////////////////////////////////
 
             // Create a Minimum Risk Route graphic
             // First create some Air Control Points to place along the route
@@ -222,9 +216,8 @@ public class Symbology extends ApplicationTemplate
 
             positions = Arrays.asList(
                 Position.fromDegrees(34.8802, -117.9773, 0),
-                Position.fromDegrees(34.9457, -117.7788, 0),
-                Position.fromDegrees(35.0358, -117.7403, 0)
-            );
+                Position.fromDegrees(35.0947, -117.9578, 0),
+                Position.fromDegrees(35.1739, -117.6957, 0));
 
             // Create an Air Control Point
             graphic = factory.createGraphic("GFFPAPP-------X", positions.get(0), null);
@@ -254,22 +247,78 @@ public class Symbology extends ApplicationTemplate
             // And add the route to the layer. Note that we do not need to add the individual control points
             // to the layer because the route will take care of drawing them.
             layer.addRenderable(graphic);
+        }
 
-            // Create a Circular Target graphic
-            Position position = (Position.fromDegrees(34.7202, -117.4278, 0));
+        protected void createAreaGraphics(RenderableLayer layer)
+        {
+            MilStd2525GraphicFactory factory = new MilStd2525GraphicFactory();
+            MilStd2525TacticalGraphic graphic;
+
+            /////////////////////////////////////////////
+            // Assembly area (2.X.2.1.3.2)
+            /////////////////////////////////////////////
+
+            List<Position> positions = Arrays.asList(
+                Position.fromDegrees(34.9130, -117.1897, 0),
+                Position.fromDegrees(34.9789, -117.1368, 0),
+                Position.fromDegrees(34.9706, -116.9900, 0),
+                Position.fromDegrees(34.9188, -116.9906, 0));
+            graphic = factory.createGraphic("GHGPGAA----AUSX", positions, null);
+            graphic.setValue(AVKey.DISPLAY_NAME, "Assembly Area");
+            graphic.setText("Atlanta");
+            layer.addRenderable(graphic);
+
+            /////////////////////////////////////////////
+            // Airfield zone (2.X.2.1.3.11)
+            /////////////////////////////////////////////
+
+            positions = Arrays.asList(
+                Position.fromDegrees(34.9001, -117.4044, 0),
+                Position.fromDegrees(34.9910, -117.3297, 0),
+                Position.fromDegrees(34.9851, -117.2224, 0),
+                Position.fromDegrees(34.9134, -117.2670, 0));
+            graphic = factory.createGraphic("GFGPGAZ----AUSX", positions, null);
+            graphic.setValue(AVKey.DISPLAY_NAME, "Airfield Zone");
+            layer.addRenderable(graphic);
+
+            /////////////////////////////////////////////
+            // Restricted Operation Zone (2.X.2.2.3.1)
+            /////////////////////////////////////////////
+
+            positions = Arrays.asList(
+                Position.fromDegrees(35.1991, -117.6241, 0),
+                Position.fromDegrees(35.2292, -117.4271, 0),
+                Position.fromDegrees(35.1998, -117.2560, 0),
+                Position.fromDegrees(35.0447, -117.2614, 0),
+                Position.fromDegrees(35.0338, -117.6387, 0));
+            graphic = factory.createGraphic("GFGPAAR----AUSX", positions, null);
+            graphic.setValue(AVKey.DISPLAY_NAME, "Restricted Operations Zone (ROZ)");
+            graphic.setText("(Unit ID)");
+            graphic.setModifier(AVKey.DATE_TIME, Arrays.asList(new Date(), new Date()));
+            graphic.setModifier(AVKey.ALTITUDE, Arrays.asList("100 FT AGL", "1000 FT AGL"));
+            layer.addRenderable(graphic);
+
+            /////////////////////////////////////////////
+            // Circular target (2.X.4.3.1.2)
+            /////////////////////////////////////////////
+
+            Position position = (Position.fromDegrees(35.1108, -117.0470, 0));
             graphic = factory.createGraphic("GHFPATC-------X", position, null);
             graphic.setText("AG9999");
             graphic.setModifier(AVKey.RADIUS, 5000.0); // Radius of the circle
             graphic.setValue(AVKey.DISPLAY_NAME, "Circular Target");
             layer.addRenderable(graphic);
 
-            // Create a Rectangular Target graphic
-            position = (Position.fromDegrees(34.7275, -118.0544, 0));
+            /////////////////////////////////////////////
+            // Rectangular target (2.X.4.3.1.1)
+            /////////////////////////////////////////////
+
+            position = (Position.fromDegrees(35.0295, -116.9290, 0));
 
             AVListImpl modifiers = new AVListImpl();
             // Length and width of the rectangle are specified as distance modifiers
-            modifiers.setValue(AVKey.WIDTH, 3000.0);
-            modifiers.setValue(AVKey.LENGTH, 5000.0);
+            modifiers.setValue(AVKey.WIDTH, 4000.0);
+            modifiers.setValue(AVKey.LENGTH, 8000.0);
 
             graphic = factory.createGraphic("GHFPATR-------X", position, modifiers);
             graphic.setText("AB0176");
@@ -280,9 +329,9 @@ public class Symbology extends ApplicationTemplate
 
     public static void main(String[] args)
     {
-        Configuration.setValue(AVKey.INITIAL_LATITUDE, 34.64);
-        Configuration.setValue(AVKey.INITIAL_LONGITUDE, -117.73);
-        Configuration.setValue(AVKey.INITIAL_ALTITUDE, 100000);
+        Configuration.setValue(AVKey.INITIAL_LATITUDE, 34.81);
+        Configuration.setValue(AVKey.INITIAL_LONGITUDE, -117.44);
+        Configuration.setValue(AVKey.INITIAL_ALTITUDE, 140000);
 
         ApplicationTemplate.start("World Wind Symbology", AppFrame.class);
     }
